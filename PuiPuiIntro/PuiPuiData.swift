c92dd9a26605b9fc1b8ef074c81a978ffd712725
Roger Chang <d9352676@gmail.com>
2021-07-25 11:22:58 +0800

    利用 page control，segmented control，button & gesture 更換內容

---
+++ PuiPuiIntro/PuiPuiData.swift
@@ -0,0 +1,30 @@
+//
+//  PuiPuiData.swift
+//  PuiPuiData
+//
+//  Created by 張又壬 on 2021/7/25.
+//
+
+import Foundation
+import UIKit
+
+struct PuiPuiData {
+    var pictureName: String
+    var name: String
+    var intro: String
+    var picture: UIImage? {
+        UIImage(named: pictureName)
+    }
+}
+
+extension PuiPuiData {
+    static var data: [PuiPuiData] {
+        [
+            PuiPuiData(pictureName: "chara01.jpeg", name: "馬鈴薯", intro: "個性悠哉，但也有無視司機駕駛的一面。若是出現需要幫助的人，有著無論甚麼狀況都會挺身而出的勇氣。喜歡紅蘿蔔。"),
+            PuiPuiData(pictureName: "chara02.jpeg", name: "西羅摩", intro: "具有比一般天竺鼠還膽小的性格。會向溫柔的駕駛撒嬌，常被捲入各種麻煩之中。仰慕如同姐姐一般的泰迪。喜歡生菜。"),
+            PuiPuiData(pictureName: "chara03.jpeg", name: "阿比", intro: "認真且好奇心旺盛。標註著新手記號，夢想著有朝一日能畢業。有著比其他的天竺鼠還要高的自尊心。害怕貓咪。"),
+            PuiPuiData(pictureName: "chara04.jpeg", name: "巧克力", intro: "爽朗又賢淑、卻有著意想不到的強而有力的體能。喜歡打扮也重視時尚。夢想是成為No.1的高級天竺鼠車車。"),
+            PuiPuiData(pictureName: "chara05.jpeg", name: "泰迪", intro: "總之就是什麼都吃、什麼都不怕的活潑女孩兒。總有驚人之舉的麻煩製造車，讓大夥兒相當頭疼。")
+        ]
+    }
+}
--- PuiPuiIntro/ViewController.swift
+++ PuiPuiIntro/ViewController.swift
@@ -9,11 +9,115 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var charaPicture: UIImageView!
+    
+    @IBOutlet weak var previousUIButton: UIButton!
+    
+    @IBOutlet weak var nextUIButton: UIButton!
+    
+    @IBOutlet weak var pageUILabel: UILabel!
+    
+    @IBOutlet weak var nameUILable: UILabel!
+    
+    @IBOutlet weak var charaIntroTextView: UITextView!
+    
+    @IBOutlet weak var pageUIPageControl: UIPageControl!
+    
+    @IBOutlet weak var segmentUISegmentedControl: UISegmentedControl!
+    
+    private var index = 0;
+    private var filpAni = FlipPage.none
+    
+    private var currentIndext: Int {
+        set {
+            if newValue < 0 {
+                index = PuiPuiData.data.count - 1
+                filpAni = .previous
+            }
+            else if newValue >= PuiPuiData.data.count {
+                index = 0
+                filpAni = .next
+            }
+            else {
+                if index < newValue {
+                    filpAni = .next
+                } else if index > newValue {
+                    filpAni = .previous
+                } else {
+                    filpAni = .none
+                }
+                index = newValue
+            }
+            updateView()
+        }
+        get {
+            return index
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        pageUIPageControl.numberOfPages = PuiPuiData.data.count
+        segmentUISegmentedControl.replaceSegments(datas: PuiPuiData.data)
+        currentIndext = 0
+//        updateView();
+    }
+    @IBAction func previousPage(_ sender: Any) {
+        currentIndext -= 1
+    }
+    
+    @IBAction func nextPage(_ sender: Any) {
+        currentIndext += 1
     }
 
-
+    @IBAction func pageControlChanged(_ sender: UIPageControl) {
+        currentIndext = sender.currentPage
+    }
+    
+    @IBAction func segmentChanged(_ sender: UISegmentedControl) {
+        currentIndext = sender.selectedSegmentIndex
+    }
+    
+    @IBAction func swipeChanged(_ sender: UISwipeGestureRecognizer) {
+        if sender.direction == .left {
+            currentIndext -= 1
+        } else {
+            currentIndext += 1
+        }
+    }
+    
+    func updateView() {
+        charaPicture.setImage(PuiPuiData.data[currentIndext].picture, dir: filpAni)
+        pageUILabel.text = String(currentIndext + 1)
+        nameUILable.text = PuiPuiData.data[currentIndext].name
+        charaIntroTextView.text = PuiPuiData.data[currentIndext].intro
+        pageUIPageControl.currentPage = currentIndext
+        segmentUISegmentedControl.selectedSegmentIndex = currentIndext
+    }
 }
 
+enum FlipPage {
+    case next
+    case previous
+    case none
+}
+
+extension UIImageView{
+    func setImage(_ image: UIImage?, dir: FlipPage) {
+        let duration = dir == .none ? 0.0 : 0.3
+        let options: UIView.AnimationOptions = dir == .next ? .transitionCurlUp : .transitionCurlDown
+        UIView.transition(with: self, duration: duration, options: options, animations: {
+            self.image = image
+        }, completion: nil)
+    }
+}
+
+extension UISegmentedControl {
+    func replaceSegments(datas: Array<PuiPuiData>) {
+        self.removeAllSegments()
+        for data in datas {
+            self.insertSegment(withTitle: data.name, at: self.numberOfSegments, animated: false)
+        }
+    }
+}
